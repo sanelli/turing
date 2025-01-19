5eb639a24bf8b447f5d7793b48b9d9cb2d793b33
Stefano Anelli <stefano.anelli@gmail.com>
2025-01-19 11:03:58 +0000

    Implemented tape

--- rust/src/machine/tape.rs
+++ rust/src/machine/tape.rs
@@ -1,9 +1,9 @@
-#[path = "./typing.rs"] mod typing;
-use typing::{TuringSymbol};
+#[path = "./typing.rs"]
+mod typing;
+use typing::TuringSymbol;
 
 #[derive(Debug, Default)]
-pub enum TuringTapeMove
-{
+pub enum TuringTapeMove {
     #[default]
     None,
     Left,
@@ -11,41 +11,93 @@ pub enum TuringTapeMove
 }
 
 #[derive(Debug, Default)]
-pub struct TuringTape
-{
+pub struct TuringTape {
     current_position: i32,
     positive: Vec<TuringSymbol>,
     negative: Vec<TuringSymbol>,
+    empty_symbol: TuringSymbol,
 }
 
-impl TuringTape
-{
-    pub fn move_head(&mut self, direction: TuringTapeMove)
-    {
+impl TuringTape {
+    pub fn new(empty_symbol: TuringSymbol) -> TuringTape {
+        TuringTape {
+            current_position: 0,
+            positive: vec![],
+            negative: vec![],
+            empty_symbol,
+        }
+    }
+
+    pub fn move_head(&mut self, direction: TuringTapeMove) {
         match direction {
-            TuringTapeMove::None => { /* Nothing to do */},
-            TuringTapeMove::Left => { self.current_position -= 1; },
-            TuringTapeMove::Right => { self.current_position += 1; },
+            TuringTapeMove::None => { /* Nothing to do */ }
+            TuringTapeMove::Left => {
+                self.current_position -= 1;
+            }
+            TuringTapeMove::Right => {
+                self.current_position += 1;
+            }
         };
     }
 
-    pub fn current_position(&self) -> i32
-    {
+    pub fn current_position(&self) -> i32 {
         self.current_position
     }
 
-    pub fn get() -> TuringSymbol{
-        todo!();
+    pub fn get(&mut self) -> TuringSymbol {
+        self.ensure_tape_sizes();
+        let (side, position) = self.get_tape_and_position_for_accessing();
+        side[position]
     }
 
-    pub fn set(symbol: TuringSymbol) {
-        todo!();
+    pub fn set(&mut self, symbol: TuringSymbol) -> () {
+        self.ensure_tape_sizes();
+        let (side, position) = self.get_tape_and_position_for_accessing();
+        side[position] = symbol;
+    }
+
+    fn ensure_tape_sizes(&mut self) {
+        let ensure: usize;
+        let side: &mut Vec<TuringSymbol>;
+
+        if self.current_position >= 0 {
+            ensure = (self.current_position + 1) as usize;
+            side = &mut self.positive;
+        } else {
+            ensure = (-self.current_position) as usize;
+            side = &mut self.negative;
+        }
+
+        Self::ensure_tape_size_for_specific_tape(side, ensure, self.empty_symbol);
+    }
+
+    fn get_tape_and_position_for_accessing(&mut self) -> (&mut Vec<TuringSymbol>, usize) {
+        let position;
+        let side;
+        if self.current_position >= 0 {
+            position = self.current_position as usize;
+            side = &mut self.positive;
+        } else {
+            position = (-self.current_position + 1) as usize;
+            side = &mut self.negative;
+        }
+
+        (side, position)
+    }
+
+    fn ensure_tape_size_for_specific_tape(
+        side: &mut Vec<TuringSymbol>,
+        ensure: usize,
+        empty_symbol: TuringSymbol,
+    ) {
+        while side.len() < ensure {
+            side.push(empty_symbol);
+        }
     }
 }
 
 #[test]
-fn test_move_head()
-{
+fn test_move_head() {
     let mut tape = TuringTape::default();
     assert_eq!(tape.current_position, 0);
     tape.move_head(TuringTapeMove::None);
@@ -57,8 +109,7 @@ fn test_move_head()
 }
 
 #[test]
-fn test_current_position()
-{
+fn test_current_position() {
     let mut tape = TuringTape::default();
     assert_eq!(tape.current_position(), 0);
     tape.move_head(TuringTapeMove::None);
